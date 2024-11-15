3cf771e3f54794d0777129ca9871730fc65af27f
CJ Naujokas <cnaujoka@charlotte.edu>
2024-11-14 19:53:59 -0500

    feat: Added review model, schema, to create database table for reviews

---
+++ api/models/reviews.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from ..dependencies.database import Base
+
+class Review(Base):
+    __tablename__ = "reviews"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    content = Column(String(100), unique=True, nullable=False)
+    stars = Column(Integer, index=True, nullable=False, server_default='0.0')
+
+    recipes = relationship("Recipe", back_populates="resource")
