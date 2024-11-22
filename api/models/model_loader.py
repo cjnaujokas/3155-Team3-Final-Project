b99c0e4d9f1ad26be6f59a0581a4be474e0b68e0
CJ Naujokas <cnaujoka@charlotte.edu>
2024-11-22 10:07:41 -0500

    fix: added promo_codes and revenue to model_loader and fixed error in models/promo_codes.py

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, recipes, food_items, resources, reviews
+from . import orders, order_details, recipes, food_items, resources, reviews, revenue, promo_codes
 
 from ..dependencies.database import engine
 
@@ -10,3 +10,5 @@ def index():
     food_items.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
     reviews.Base.metadata.create_all(engine)
+    revenue.Base.metadata.create_all(engine)
+    promo_codes.Base.metadata.create_all(engine)
