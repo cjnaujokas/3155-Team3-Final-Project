bb0a53423a40ed790265b953e0a2a13e9fed5490
Christopher Nash <cnash8@uncc.edu>
2024-11-22 17:41:51 -0500

    feat

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, recipes, food_items, resources, reviews
+from . import orders, order_details, recipes, food_items, resources, reviews, complete_orders, users, promo_codes, revenue
 
 from ..dependencies.database import engine
 
@@ -10,3 +10,7 @@ def index():
     food_items.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
     reviews.Base.metadata.create_all(engine)
+    complete_orders.Base.metadata.create_all(engine)
+    users.Base.metadata.create_all(engine)
+    promo_codes.Base.metadata.create_all(engine)
+    revenue.Base.metadata.create_all(engine)
