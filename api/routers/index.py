5b55091bb2d002f814076f195adc9d8ee7fdaa43
CJ Naujokas <cnaujoka@charlotte.edu>
2024-12-03 13:59:32 -0500

    fix: Changed order of routers

--- api/routers/index.py
+++ api/routers/index.py
@@ -3,14 +3,14 @@ from . import orders, order_details, reviews, users, complete_orders, revenue, r
 
 def load_routes(app):
     app.include_router(orders.router)
-    app.include_router(order_details.router)
-    app.include_router(reviews.router)
+    app.include_router(food_items.router)
     app.include_router(users.router)
+    app.include_router(order_details.router)
     app.include_router(complete_orders.router)
+    app.include_router(reviews.router)
     app.include_router(revenue.router)
     app.include_router(resources.router)
     app.include_router(promo_codes.router)
-    app.include_router(food_items.router)
     app.include_router(payment.router)
     app.include_router(recipes.router)
     
